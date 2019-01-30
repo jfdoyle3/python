52f02dd9657590b8d13014793f06a0f4edf72df6
Jim Doyle <indellibleink@gmail.com>
2019-01-30 09:02:28 -0500

    fixed typos in comments

--- basicDataTypes.py
+++ basicDataTypes.py
@@ -1,39 +1,39 @@
 # Print to console
-print ('resistance is futile!');
+print('resistance is futile!')
 # Math print to console
-print (99999999999*99999999999)
+print(99999999999*99999999999)
 
-print ('<---------->')
-# semicolon at EOL aren't nessescary
+print('<---------->')
+# semicolon at EOL aren't nesscesscary;
 # this is a valid usage. Still prints on 2 lines
-print ('4*3='); print (4*3)
-print ('<---------->')
-## Basic python data types
-## ------------------------
-## Interger decimal: type  int
-a=2
-## Floating decimal: type float
-b=2.5
-## booleans type bool  True or False
-c=True
-## None value
-d=None
+print('4*3=')
+print(4*3)
+print('<---------->')
+# Basic python data types
+# ------------------------
+# Interger decimal: type  int
+a = 2
+# Floating decimal: type float
+b = 2.5
+# booleans type bool  True or False
+c = True
+# None value
+d = None
 # Print results
-print (a,b,c,d)
-# use type(var) to retreive varible data type
-print (type(a),type(b),type(c),type(d))
-print ('<---------->')
+print(a, b, c, d)
+# use type(var) to retrieve variable data type
+print(type(a), type(b), type(c), type(d))
+print('<---------->')
 # is:  function
-print ('d = None')
-print ('d is None:')
-print (d is None)
-print ('<---------->')
-print ('a = 2')
-print ('a is None:')
-print (a is None)
-print ('<---------->')
+print('d = None')
+print('d is None:')
+print(d is None)
+print('<---------->')
+print('a = 2')
+print('a is None:')
+print(a is None)
+print('<---------->')
 # is not: Function
-print ('a = 2')
-print ('a is not None:')
-print (a is not None)
-
+print('a = 2')
+print('a is not None:')
+print(a is not None)
